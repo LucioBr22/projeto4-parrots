2eb37a8e4b7b8f56761c72076d476b1995462e64
Lucio <lucio.bruno.printes@hotmail.com>
2023-01-13 19:27:28 -0300

    Randomizando os gifs pelas cartas

--- script.js
+++ script.js
@@ -10,6 +10,51 @@ while (true){
 
 const conjuntoCartas = document.querySelector('.conjunto-cartas');
 
+let listaGifs = [
+    'bobrossparrot.gif',
+    'bobrossparrot.gif',
+    'explodyparrot.gif',
+    'explodyparrot.gif',
+    'fiestaparrot.gif',
+    'fiestaparrot.gif',
+    'metalparrot.gif',
+    'metalparrot.gif',
+    'revertitparrot.gif',
+    'revertitparrot.gif',
+    'tripletsparrot.gif',
+    'tripletsparrot.gif',
+    'unicornparrot.gif',
+    'unicornparrot.gif'
+]
+
+listaGifs.length = quantidadeCartas;
+
 for(let i=0;i<quantidadeCartas;i++){
-    conjuntoCartas.innerHTML = conjuntoCartas.innerHTML + `<div class='cartas'><img class ='fundo' src='images/back.png'/><img class ='frente' src='gifs/bobrossparrot.gif'></div>`;
+
+    let random = (min,max) => Math.floor(Math.random()*(max - min) + min);
+    let nomeDoGif = listaGifs[random(0,listaGifs.length)];
+    listaGifs.splice(listaGifs.indexOf(nomeDoGif),1);
+
+    conjuntoCartas.innerHTML = conjuntoCartas.innerHTML + `<div class='cartas'><img class ='fundo' src='images/back.png'/><img class ='frente' src='gifs/${nomeDoGif}'></div>`;
 }
+
+console.log(listaGifs);
+
+const todasCartas = document.querySelectorAll('.cartas');
+
+let virouACarta = false;
+
+let primeiraCarta, segundaCarta;
+
+function virarCarta(){
+    this.classList.add('virar');
+
+    if(!virouACarta){
+        virouACarta = true;
+        primeiraCarta = this;
+    }
+}
+
+todasCartas.forEach(cartas => cartas.addEventListener('click',virarCarta));
+
+console.log(todasCartas);
